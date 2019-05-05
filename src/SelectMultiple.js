3836b27b97d28e6f5d3f96a403862fc774db36ae
Zachary Margolies <zachary.margolies@gmail.com>
2019-05-05 11:13:58 -0400

    Initial commit. Copied package edits from project into this forked repo.

--- src/SelectMultiple.js
+++ src/SelectMultiple.js
@@ -1,37 +1,42 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { View, FlatList, Text, TouchableWithoutFeedback, Image } from 'react-native'
-import styles from './SelectMultiple.styles'
-import checkbox from '../images/icon-checkbox.png'
-import checkboxChecked from '../images/icon-checkbox-checked.png'
-import { mergeStyles } from './style'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  View,
+  ListView,
+  Text,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
+import styles from './SelectMultiple.styles';
+import checkbox from '../images/icon-checkbox.png';
+import checkboxChecked from '../images/icon-checkbox-checked.png';
+import { mergeStyles } from './style';
 
 const itemType = PropTypes.oneOfType([
   PropTypes.string,
-  PropTypes.shape({ label: PropTypes.any, value: PropTypes.any })
-])
+  PropTypes.shape({ content: PropTypes.any, id: PropTypes.any }),
+]);
 
 const styleType = PropTypes.oneOfType([
   PropTypes.object,
   PropTypes.number,
-  PropTypes.array
-])
+  PropTypes.array,
+]);
 
-const sourceType = PropTypes.oneOfType([PropTypes.object, PropTypes.number])
+const sourceType = PropTypes.oneOfType([PropTypes.object, PropTypes.number]);
 
-// A customiseable FlatList that allows you to select multiple rows
+// A customiseable ListView that allows you to select multiple rows
 export default class SelectMultiple extends Component {
   static propTypes = {
     items: PropTypes.arrayOf(itemType).isRequired,
     selectedItems: PropTypes.arrayOf(itemType),
 
     onSelectionsChange: PropTypes.func.isRequired,
-    keyExtractor: PropTypes.func,
 
     checkboxSource: sourceType,
     selectedCheckboxSource: sourceType,
     renderLabel: PropTypes.func,
-    flatListProps: PropTypes.any,
+    listViewProps: PropTypes.any,
     style: styleType,
     rowStyle: styleType,
     checkboxStyle: styleType,
@@ -39,8 +44,8 @@ export default class SelectMultiple extends Component {
 
     selectedRowStyle: styleType,
     selectedCheckboxStyle: styleType,
-    selectedLabelStyle: styleType
-  }
+    selectedLabelStyle: styleType,
+  };
 
   static defaultProps = {
     selectedItems: [],
@@ -51,112 +56,124 @@ export default class SelectMultiple extends Component {
     labelStyle: {},
     checkboxSource: checkbox,
     selectedCheckboxSource: checkboxChecked,
-    renderLabel: null
+    renderLabel: null,
+  };
+
+  constructor(props) {
+    super(props);
+
+    const rows = this.getRowData(props);
+
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1.id !== r2.id || r1.selected !== r2.selected,
+    }).cloneWithRows(rows);
+
+    this.state = { dataSource };
   }
 
-  constructor (props) {
-    super(props)
-
-    this.state = { dataSource: [] }
+  componentWillReceiveProps(nextProps) {
+    const rows = this.getRowData(nextProps);
+    const dataSource = this.state.dataSource.cloneWithRows(rows);
+    this.setState({ dataSource });
   }
 
-  componentWillReceiveProps (nextProps) {
-    const rows = this.getRowData(nextProps)
-    this.setState({ dataSource: rows })
+  getRowData({ items, selectedItems }) {
+    items = items.map(this.toLabelValueObject);
+    selectedItems = (selectedItems || []).map(this.toLabelValueObject);
+
+    items.forEach(item => {
+      item.selected = selectedItems.some(i => i.id === item.id);
+    });
+
+    return items;
   }
 
-  getRowData ({ items, selectedItems }) {
-    items = items.map(this.toLabelValueObject)
-    selectedItems = (selectedItems || []).map(this.toLabelValueObject)
+  onRowPress(row) {
+    const { content, id } = row;
+    let { selectedItems } = this.props;
 
-    items.forEach((item) => {
-      item.selected = selectedItems.some((i) => i.value === item.value)
-    })
+    selectedItems = (selectedItems || []).map(this.toLabelValueObject);
 
-    return items
-  }
-
-  onRowPress (row) {
-    const { label, value } = row
-    let { selectedItems } = this.props
-
-    selectedItems = (selectedItems || []).map(this.toLabelValueObject)
-
-    const index = selectedItems.findIndex((selectedItem) => selectedItem.value === value)
+    const index = selectedItems.findIndex(
+      selectedItem => selectedItem.id === id
+    );
 
     if (index > -1) {
-      selectedItems = selectedItems.filter((selectedItem) => selectedItem.value !== value)
+      selectedItems = selectedItems.filter(
+        selectedItem => selectedItem.id !== id
+      );
     } else {
-      selectedItems = selectedItems.concat({ label, value })
+      selectedItems = selectedItems.concat({ content, id });
     }
 
-    this.props.onSelectionsChange(selectedItems, { label, value })
+    this.props.onSelectionsChange(selectedItems, { content, id });
   }
 
-  toLabelValueObject (obj) {
+  toLabelValueObject(obj) {
     if (Object.prototype.toString.call(obj) === '[object String]') {
-      return { label: obj, value: obj }
+      return { content: obj, id: obj };
     } else {
-      return { label: obj.label, value: obj.value }
+      return { content: obj.content, id: obj.id };
     }
   }
 
-  keyExtractor = (item, index) => index
-
-  render () {
-    const { dataSource } = this.state
-    const { style, flatListProps, keyExtractor } = this.props
-    return <FlatList
-      style={style}
-      keyExtractor={keyExtractor || this.keyExtractor}
-      data={dataSource}
-      renderItem={this.renderItemRow}
-      {...flatListProps}
-    />
+  render() {
+    const { dataSource } = this.state;
+    const { style, listViewProps } = this.props;
+    const { renderItemRow } = this;
+    return (
+      <ListView
+        style={style}
+        dataSource={dataSource}
+        renderRow={renderItemRow}
+        {...listViewProps || {}}
+      />
+    );
   }
 
-  renderLabel = (label, style, selected) => {
+  renderLabel = (content, style, selected) => {
     if (this.props.renderLabel) {
-      return this.props.renderLabel(label, style, selected)
+      return this.props.renderLabel(content, style, selected);
     }
     return (
-      <Text style={style}>{label}</Text>
-    )
-  }
+      <Text numberOfLines={2} style={style}>
+        {content}
+      </Text>
+    );
+  };
 
-  renderItemRow = (row) => {
-    let {
-      checkboxSource,
-      rowStyle,
-      labelStyle,
-      checkboxStyle
-    } = this.props
+  renderItemRow = row => {
+    let { checkboxSource, rowStyle, labelStyle, checkboxStyle } = this.props;
 
     const {
       selectedCheckboxSource,
       selectedRowStyle,
       selectedCheckboxStyle,
-      selectedLabelStyle
-    } = this.props
+      selectedLabelStyle,
+    } = this.props;
 
-    if (row.item.selected) {
-      checkboxSource = selectedCheckboxSource
-      rowStyle = mergeStyles(styles.row, rowStyle, selectedRowStyle)
-      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle, selectedCheckboxStyle)
-      labelStyle = mergeStyles(styles.label, labelStyle, selectedLabelStyle)
+    if (row.selected) {
+      checkboxSource = selectedCheckboxSource;
+      rowStyle = mergeStyles(styles.row, rowStyle, selectedRowStyle);
+      checkboxStyle = mergeStyles(
+        styles.checkbox,
+        checkboxStyle,
+        selectedCheckboxStyle
+      );
+      labelStyle = mergeStyles(styles.label, labelStyle, selectedLabelStyle);
     } else {
-      rowStyle = mergeStyles(styles.row, rowStyle)
-      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle)
-      labelStyle = mergeStyles(styles.label, labelStyle)
+      rowStyle = mergeStyles(styles.row, rowStyle);
+      checkboxStyle = mergeStyles(styles.checkbox, checkboxStyle);
+      labelStyle = mergeStyles(styles.label, labelStyle);
     }
 
     return (
-      <TouchableWithoutFeedback onPress={() => this.onRowPress(row.item)}>
+      <TouchableWithoutFeedback onPress={() => this.onRowPress(row)}>
         <View style={rowStyle}>
           <Image style={checkboxStyle} source={checkboxSource} />
-          {this.renderLabel(row.item.label, labelStyle, row.item.selected)}
+          {this.renderLabel(row.content, labelStyle, row.selected)}
         </View>
       </TouchableWithoutFeedback>
-    )
-  }
+    );
+  };
 }
